54be1eb0d8d43d1ce1ac169a1b3f142c908b6804
Charles Sarrazin <charles@sarraz.in>
2014-12-27 20:09:49 +0100

    Fixed unit tests

--- dist/Tests/DataCollector/GuzzleCollectorTest.php
+++ dist/Tests/DataCollector/GuzzleCollectorTest.php
@@ -3,67 +3,36 @@
 namespace Csa\Bundle\GuzzleBundle\Tests\DataCollector;
 
 use Csa\Bundle\GuzzleBundle\DataCollector\GuzzleCollector;
+use Csa\Bundle\GuzzleBundle\Factory\Client;
+use Csa\Bundle\GuzzleBundle\GuzzleHttp\Subscriber\DebugSubscriber;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Subscriber\Mock;
 use Symfony\Component\HttpFoundation\Request;
 
 class GuzzleCollectorTest extends \PHPUnit_Framework_TestCase
 {
     public function testCollect()
     {
-        $history = $this->getHistoryMock();
-        $collector = new GuzzleCollector($history);
+        $mocks = array_fill(0, 3, new Response(204));
+
+        $mockSubscriber = new Mock($mocks);
+        $client = new Client();
+        $client->getEmitter()->attach($mockSubscriber);
+        $debugSubscriber = new DebugSubscriber();
+        $client->getEmitter()->attach($debugSubscriber);
+        $collector = new GuzzleCollector($debugSubscriber);
 
         $request = Request::createFromGlobals();
         $response = $this->getMock('Symfony\Component\HttpFoundation\Response');
         $collector->collect($request, $response, new \Exception());
+        $this->assertCount(0, $collector->getCalls());
+
+        $client->get('http://foo.bar');
+        $collector->collect($request, $response, new \Exception());
         $this->assertCount(1, $collector->getCalls());
-    }
 
-    private function getHistoryMock()
-    {
-        $history  = $this
-            ->getMockBuilder('Csa\Bundle\GuzzleBundle\GuzzleHttp\Subscriber\DebugSubscriber')
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
-
-        $request  = $this
-            ->getMockBuilder('GuzzleHttp\Message\Request')
-            ->disableOriginalConstructor()
-            ->setMethods(['getMethod', 'getProtocolVersion', 'getUrl', 'getHeaders', 'getBody'])
-            ->getMock()
-        ;
-
-        $request->expects($this->once())->method('getMethod');
-        $request->expects($this->once())->method('getProtocolVersion');
-        $request->expects($this->once())->method('getUrl');
-        $request->expects($this->once())->method('getHeaders');
-        $request->expects($this->once())->method('getBody');
-
-        $response = $this
-            ->getMockBuilder('GuzzleHttp\Message\Response')
-            ->disableOriginalConstructor()
-            ->setMethods(['getStatusCode', 'getReasonPhrase', 'getEffectiveUrl', 'getHeaders', 'getBody'])
-            ->getMock()
-        ;
-
-        $response->expects($this->once())->method('getStatusCode');
-        $response->expects($this->once())->method('getReasonPhrase');
-        $response->expects($this->once())->method('getEffectiveUrl');
-        $response->expects($this->once())->method('getHeaders');
-        $response->expects($this->once())->method('getBody');
-
-        $history
-            ->expects($this->once())
-            ->method('getIterator')
-            ->will($this->returnValue(new \ArrayIterator([
-                [
-                    'request'  => $request,
-                    'response' => $response,
-                    'duration' => 100,
-                ]
-            ])))
-        ;
-
-        return $history;
+        $client->get('http://foo.bar');
+        $collector->collect($request, $response, new \Exception());
+        $this->assertCount(2, $collector->getCalls());
     }
 }
