7f7112bf03b7e19d4bf666910ab3d8e5175cb8e0
Yannick Le Guédart <yannick@overblog.com>
2015-12-10 14:39:07 +0100

    Fix cache error loosing response body.

--- src/GuzzleHttp/Middleware.php
+++ src/GuzzleHttp/Middleware.php
@@ -55,6 +55,8 @@ class Middleware
                         function (ResponseInterface $response) use ($adapter, $request, $debug) {
                             $adapter->save($request, $response);
 
+                            $response->getBody()->seek(0);
+
                             if ($debug) {
                                 $response = $response->withHeader('X-Guzzle-Cache', 'MISS');
                             }
