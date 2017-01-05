a60b534fc334ad2152f2216217f247f1f2c2c231
iamluc <luc@vieillescazes.net>
2017-01-05 15:36:01 +0100

    Fix GuzzleCollector when the response is not an instance of ResponseInterface (Can be an Exception or anything)

--- src/DataCollector/GuzzleCollector.php
+++ src/DataCollector/GuzzleCollector.php
@@ -15,6 +15,7 @@ use Csa\Bundle\GuzzleBundle\GuzzleHttp\History\History;
 use Csa\Bundle\GuzzleBundle\GuzzleHttp\Middleware\CacheMiddleware;
 use Csa\Bundle\GuzzleBundle\GuzzleHttp\Middleware\MockMiddleware;
 use GuzzleHttp\Exception\RequestException;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -84,7 +85,7 @@ class GuzzleCollector extends DataCollector
                 $req['curl'] = $this->curlFormatter->format($request);
             }
 
-            if ($response) {
+            if ($response instanceof ResponseInterface) {
                 $req['response'] = [
                     'reasonPhrase' => $response->getReasonPhrase(),
                     'headers' => $response->getHeaders(),
