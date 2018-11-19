60f0fc184460ff1641f010a2dc2b811678eea52c
Thiago Cordeiro <thiago.cordeiro@check24.de>
2018-11-19 14:25:25 +0100

    Testing previous existing code

--- tests/Twig/Extension/GuzzleExtensionTest.php
+++ tests/Twig/Extension/GuzzleExtensionTest.php
@@ -47,6 +47,33 @@ STRING;
         $this->extension = new GuzzleExtension();
     }
 
+    public function testGetFilters(): void
+    {
+        $filters = $this->extension->getFilters();
+
+        $names = array_map(function (Twig_Filter $filter) {
+            return $filter->getName();
+        }, $filters);
+
+        $this->assertEquals([
+            'csa_guzzle_pretty_print',
+            'csa_guzzle_status_code_class',
+            'csa_guzzle_format_duration',
+            'csa_guzzle_short_uri',
+        ], $names);
+    }
+
+    public function testFetFunctions(): void
+    {
+        $functions = $this->extension->getFunctions();
+
+        $names = array_map(function (Twig_SimpleFunction $filter) {
+            return $filter->getName();
+        }, $functions);
+
+        $this->assertEquals(['csa_guzzle_detect_lang'], $names);
+    }
+
     /**
      * @dataProvider langBodyDataSet
      */
@@ -65,6 +92,42 @@ STRING;
         $rendered = $this->extension->prettyPrint($body, $lang);
 
         $this->assertEquals(trim($pretty), trim($rendered));
+    }
+
+    /**
+     * @dataProvider statusCodeMessageDataSet
+     */
+    public function testStatusCodeClass(int $statusCode, string $expectedMessage): void
+    {
+        $message = $this->extension->statusCodeClass($statusCode);
+
+        $this->assertEquals($expectedMessage, $message);
+    }
+
+    /**
+     * @dataProvider formatDurationDataSet
+     */
+    public function testFormatDuration(int $seconds, string $expectedFormatted): void
+    {
+        $formatted = $this->extension->formatDuration($seconds);
+
+        $this->assertEquals($expectedFormatted, $formatted);
+    }
+
+    public function testShortenUri(): void
+    {
+        $uri = 'http://localhost/foo/bar?foo=bar';
+
+        $short = $this->extension->shortenUri($uri);
+
+        $this->assertEquals('http://localhost', $short);
+    }
+
+    public function testGetName(): void
+    {
+        $name = $this->extension->getName();
+
+        $this->assertEquals('csa_guzzle', $name);
     }
 
     public function langBodyDataSet(): array
@@ -95,6 +158,35 @@ STRING;
                 'body'   => 'body;test',
                 'pretty' => self::PRETTY_MARKUP,
             ],
+        ];
+    }
+
+    public function statusCodeMessageDataSet(): array
+    {
+        return [
+            'status 599' => ['statusCode' => 599, 'expectedMessage' => 'server-error'],
+            'status 500' => ['statusCode' => 500, 'expectedMessage' => 'server-error'],
+            'status 499' => ['statusCode' => 499, 'expectedMessage' => 'client-error'],
+            'status 400' => ['statusCode' => 400, 'expectedMessage' => 'client-error'],
+            'status 399' => ['statusCode' => 399, 'expectedMessage' => 'redirection'],
+            'status 300' => ['statusCode' => 300, 'expectedMessage' => 'redirection'],
+            'status 299' => ['statusCode' => 299, 'expectedMessage' => 'success'],
+            'status 200' => ['statusCode' => 200, 'expectedMessage' => 'success'],
+            'status 199' => ['statusCode' => 199, 'expectedMessage' => 'informational'],
+            'status 100' => ['statusCode' => 100, 'expectedMessage' => 'informational'],
+            'status 99'  => ['statusCode' => 99, 'expectedMessage' => 'unknown'],
+            'status 0'   => ['statusCode' => 0, 'expectedMessage' => 'unknown'],
+        ];
+    }
+
+    public function formatDurationDataSet(): array
+    {
+        return [
+            '15 Seconds'   => ['seconds' => 15, 'formatted' => '15.00 s'],
+            '92 Seconds'   => ['seconds' => 92, 'formatted' => '92.00 s'],
+            '136 Seconds'  => ['seconds' => 136, 'formatted' => '136.00 s'],
+            '532 Seconds'  => ['seconds' => 532, 'formatted' => '532.00 s'],
+            '1689 Seconds' => ['seconds' => 1689, 'formatted' => '1689.00 s'],
         ];
     }
 }
